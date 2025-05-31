842fbb2fca0da482b8ad3ab7e90ada2627f6bb51
rahul-saini-io <rahulsaini311292@gmail.com>
2025-05-31 12:58:13 +0530

    valid subsequence

---
+++ valid_subsequence.py
@@ -0,0 +1,19 @@
+def if_valid_subseq(a, b):
+    b_idx = 0
+
+    for val in a:
+        if b[b_idx] == val:
+            b_idx += 1
+        if b_idx == len(b):
+            # valid subsequence
+            return True
+
+    return False
+
+
+
+
+a = [1,2,3,4,5,10]
+b = [2,3,5]
+
+print(if_valid_subseq(a, b))
