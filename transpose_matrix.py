f9330b1b61ad84c9b3e9c8d1e667785d0a46a5c7
rahul-saini-io <rahulsaini311292@gmail.com>
2025-05-31 19:19:37 +0530

    transpose matrix

--- transpose_matrix.py
+++ transpose_matrix.py
@@ -3,14 +3,12 @@ def transpose(matrix):
     cols = len(matrix[0])
 
     result = [[0] * rows for _ in range(cols) ]
-    print(result)
+
     for i in range(rows):
         for j in range(cols):
             result[j][i]=matrix[i][j]
-            print(result)
 
-
-    # return result
+    return result
 
 
 matrix = [
